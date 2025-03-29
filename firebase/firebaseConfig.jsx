0c380b798af0c8199c9de1337c13eb95c5b4fa8c
ajfeng4 <ajfeng@buffalo.edu>
2025-03-28 21:19:06 -0400

    Setup firebase db

---
+++ firebase/firebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyADXtiXzJF42vOXFCe-wvWYn-c0f60z2NU",
+    authDomain: "impairment-67cd9.firebaseapp.com",
+    projectId: "impairment-67cd9",
+    storageBucket: "impairment-67cd9.firebasestorage.app",
+    messagingSenderId: "879562139368",
+    appId: "1:879562139368:web:70e53acf5048d17d97cb5d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
