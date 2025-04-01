983a58145b145d4738973c01b1167318bec375c4
noahschn05 <noahs0510@gmail.com>
2025-04-01 18:22:04 -0400

    Update firebaseConfig.jsx

--- firebase/firebaseConfig.jsx
+++ firebase/firebaseConfig.jsx
@@ -11,12 +11,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCMEquo_Xw9ZSIeT-PWGM0M6FVGMp1xlOM",
-  authDomain: "test-c94be.firebaseapp.com",
-  projectId: "test-c94be",
-  storageBucket: "test-c94be.firebasestorage.app",
-  messagingSenderId: "389345972092",
-  appId: "1:389345972092:web:1423ac17ace5c91a2adef9"
 };
 
 // Initialize Firebase
