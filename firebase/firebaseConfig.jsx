ccc153ac238fff3924457bf8f3f627f64810688b
Rohin Kumar <76756199+MayoMan7@users.noreply.github.com>
2025-04-02 18:17:04 -0400

    Revert "Merge pull request #1 from UB-AppDev/RohinsWork"
    This reverts commit 84bca2c92719bcf69cd497a21dfe2151804fe98a, reversing
    changes made to 855871a328bfd85c84b2d8653fc6147ad7317325.

--- firebase/firebaseConfig.jsx
+++ firebase/firebaseConfig.jsx
@@ -1,41 +1,29 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"; 
-// Have to do ts-ignore as getReactNativePersistence is not detected by ts compiler with firebase 10.3.0
-// @ts-ignore 
+import { initializeApp } from "firebase/app";
 import {
     initializeAuth,
     getReactNativePersistence
 } from 'firebase/auth';
-import {
-    FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID,
-  } from "@env";
 
-
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: FIREBASE_API_KEY,
-    authDomain: FIREBASE_AUTH_DOMAIN,
-    projectId: FIREBASE_PROJECT_ID,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-    appId: FIREBASE_APP_ID,
+    //apiKey: [YOUR FIREBASE API KEY],
+    //authDomain: [YOUR AUTH DOMAIN],
+    //projectId: [YOUR PROJECT ID],
+    //storageBucket: [YOUR STORAGE BUCKET],
+    //messagingSenderId: [YOUR MESSAGING SENDER ID],
+    //appId: [YOUR APP ID]
 };
-  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+    persistence: getReactNativePersistence(AsyncStorage),
 });
 
 export { auth };
